736f815c4e17526e8718a179408a1c7b161bcb25
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-10-26 14:17:47 +0100

    Cleans Imports

--- app/src/main/java/com/example/practice_superherolistapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/practice_superherolistapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.practice_superherolistapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme

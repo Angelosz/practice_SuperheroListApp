732e3751b5e5db19240b63c43ae2a40688b3122b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-10-26 14:18:00 +0100

    Adds TopBar

--- app/src/main/java/com/example/practice_superherolistapp/MainActivity.kt
+++ app/src/main/java/com/example/practice_superherolistapp/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,11 +26,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practice_superherolistapp.data.SuperHero
@@ -38,7 +46,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Practice_SuperheroListAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(WindowInsets.statusBars.asPaddingValues())
+                            .padding(bottom = dimensionResource(R.dimen.padding_medium)),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center) {
+                            Text(
+                                text = stringResource(R.string.superheroes),
+                                style = MaterialTheme.typography.displayLarge,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }) { innerPadding ->
                     SuperHeroCardList(SuperHeroRepository.superHeroes, Modifier.padding(innerPadding))
                 }
             }
@@ -46,10 +68,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun SuperHeroCard(superHero: SuperHero, modifier: Modifier = Modifier) {
-    Card(modifier){
+    Card(modifier.shadow(elevation = 2.dp, shape = MaterialTheme.shapes.medium)
+    ){
         Row(modifier = Modifier
             .padding(dimensionResource(R.dimen.padding_medium))
             .fillMaxWidth()
@@ -63,7 +85,7 @@ fun SuperHeroCard(superHero: SuperHero, modifier: Modifier = Modifier) {
                 Text(
                     text = stringResource(superHero.descriptionRes),
                     style = MaterialTheme.typography.labelSmall,
-                    modifier = Modifier.padding(top = 4.dp)
+                    modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
                 )
             }
             Spacer(modifier = Modifier.weight(1f))
@@ -112,7 +134,20 @@ fun SuperHeroCardPreview(){
 @Composable
 fun SuperHeroCardListPreview(){
     Practice_SuperheroListAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_medium)),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center) {
+                    Text(
+                        text = stringResource(R.string.superheroes),
+                        style = MaterialTheme.typography.displayLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }) { innerPadding ->
             SuperHeroCardList(SuperHeroRepository.superHeroes, Modifier.padding(innerPadding))
         }
     }
